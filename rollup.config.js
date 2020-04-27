746757aa5833e70b5c5a86523dd39553808a26ae
JetLu <i@lufei.im>
2020-04-28 01:18:02 +0800

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
-import common from 'rollup-plugin-commonjs'
 import {terser} from 'rollup-plugin-terser'
-import resolve from 'rollup-plugin-node-resolve'
+import common from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'src/index.js',
